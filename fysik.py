92ae3a2bd6cd8068e1f483a2915d9c392d0a4ac3
maltekasemo <117286654+maltekasemo@users.noreply.github.com>
2023-04-11 14:35:56 +0200

    Update fysik.py

--- fysik.py
+++ fysik.py
@@ -4,27 +4,29 @@ from scipy.integrate import solve_ivp
 import numpy as np
 
 
-t_span = (0, 30)
-start = [1, 0, 0, 1/2]
+t_span = (0, 300) #antalet sekunder som varvas
+start = [1, 0, 0, sqrt(2)] #startens förutsättningar
 
-def f(t, r):
+def f(t, state): #the function takes a vector of the actual state of the particles.
+    # It returns the velcoity and acceleration acting on the particle
+
     #0 x position
     #1 y position
     #2 velocity in x
     #3 velocity in y
-    x, y, vx, vy = r
+    x, y, vx, vy = state
 
-    fvx = - (x / (x**2 + y**2) ** (3 / 2))
-    fvy = - (y / (x**2 + y**2) ** (3 / 2))
+    ax = - x / (x ** 2 + y ** 2) ** (3/2)
+    ay = - y / (x ** 2 + y ** 2) ** (3/2)
 
-    return (vx, vy, fvx, fvy)
+    return (vx, vy, ax, ay)
 
     #0 x velocity
     #1 y velocity
     #2 acceleration in x
     #3 acceleration in y
 
-sol = solve_ivp(f, t_span, start, t_eval = np.linspace(0, 30, 300))
+sol = solve_ivp(f, t_span, start, t_eval=np.arange(0, 300 + 0.01, 0.01), method ='LSODA')
 
 x, y, vx, vy = sol.y
 
@@ -32,7 +34,7 @@ x, y, vx, vy = sol.y
 potential_energy = - 1 / np.sqrt(x**2 + y**2)
 
 # Calculate kinetic energy
-kinetic_energy = 0.5 * (vx**2 + vy**2)
+kinetic_energy = (vx**2 + vy**2) / 2
 
 # Calculate total energy
 total_energy = kinetic_energy + potential_energy
@@ -68,3 +70,4 @@ plt.title('Total Energy vs. Time')
 plt.legend()
 
 plt.show()
+
