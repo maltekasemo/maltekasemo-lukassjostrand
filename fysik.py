e44484b63849758d92772c25c69280801c01722f
maltekasemo <117286654+maltekasemo@users.noreply.github.com>
2023-05-03 13:03:20 +0200

    Update fysik.py

--- fysik.py
+++ fysik.py
@@ -1,23 +1,22 @@
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 import numpy as np
+from math import sqrt
 
-
-t_span = (0, 300) #antalet sekunder som varvas
-start = [1, 0, 0, 2**0.5] #startens förutsättningar
+t_span = (0, 30) #the number of seconds we iterate over
 
 def f(t, state): #the function takes a vector of the actual state of the particles.
-    # It returns the velcoity and acceleration acting on the particle
+    # It returns the velocity and acceleration acting on the particle
 
     #0 x position
-    #1 y position
+    #1 y positiont
     #2 velocity in x
     #3 velocity in y
     x, y, vx, vy = state
 
-    r = np.sqrt(x ** 2 + y ** 2)
-    ax = -x / r ** 3
-    ay = -y / r ** 3
+    r = np.sqrt(x ** 2 + y ** 2) #calculating the distance from the energy source
+    ax = -x / r ** 3 #acceleration in x direction
+    ay = -y / r ** 3 #acceleration in y direction
 
     return (vx, vy, ax, ay)
 
@@ -26,53 +25,35 @@ def f(t, state): #the function takes a vector of the actual state of the particl
     #2 acceleration in x
     #3 acceleration in y
 
-sol = solve_ivp(f, t_span, start, t_eval=np.arange(0, 300 + 0.01, 0.01), method ='LSODA')
+for start_vel in [1/2, 1, sqrt(2), 2]: #using all the velocities given in the PDF
+    start = [1, 0, 0, start_vel] #setting the initial state of movement
+    sol = solve_ivp(f, t_span, start, t_eval=np.arange(0, t_span[1], 0.001), method='LSODA') #integrating
 
-x, y, vx, vy = sol.y
+    x, y, vx, vy = sol.y #extracting the wanted parts of the answer returned
 
-# Calculate potential energy
-potential_energy = - 1 / np.sqrt(x**2 + y**2)
+    # Calculate potential energy
+    potential_energy = - 1 / np.sqrt(x**2 + y**2)
 
-# Calculate kinetic energy
-kinetic_energy = (vx**2 + vy**2) / 2
+    # Calculate kinetic energy
+    kinetic_energy = (vx**2 + vy**2) / 2
 
-# Calculate total energy
-total_energy = potential_energy + kinetic_energy
+    # Calculate total energy
+    total_energy = potential_energy + kinetic_energy
 
-# Plot x-y trajectory
-plt.figure(figsize=(8, 6))
-plt.plot(x, y)
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Trajectory of x-y')
+    # Plot x-y trajectory
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y)
+    plt.xlabel('X-coordinate')
+    plt.ylabel('Y-coordinate')
+    plt.title(f'Trajectory of x-y | vy0 = {start_vel}')
+    plt.savefig(f'trajectory_{start_vel}.png')
+    plt.clf() #clearing the figure
 
-# Plot potential energy
-plt.figure(figsize=(8, 6))
-plt.plot(sol.t, potential_energy)
-plt.xlabel('Time')
-plt.ylabel('Potential Energy')
-plt.title('Potential Energy vs. Time')
-
-# Plot kinetic energy
-plt.figure(figsize=(8, 6))
-plt.plot(sol.t, kinetic_energy, label='Kinetic Energy')
-plt.xlabel('Time')
-plt.ylabel('Kinetic Energy')
-plt.title('Kinetic Energy vs. Time')
-plt.legend()
-
-# Plot total energy
-plt.figure(figsize=(8, 6))
-plt.plot(sol.t, total_energy, label='Total Energy')
-plt.xlabel('Time')
-plt.ylabel('Total Energy')
-plt.title('Total Energy vs. Time')
-plt.legend()
-
-plt.plot(sol.t, potential_energy, label='Potential Energy')
-plt.plot(sol.t, kinetic_energy, label='Kinetic Energy')
-plt.plot(sol.t, total_energy, label='Total Energy')
-plt.legend()
-plt.show()
-
-plt.show()
+    # Plot the different energies in relationship to each other
+    plt.plot(sol.t, potential_energy, label='Potential Energy')
+    plt.plot(sol.t, kinetic_energy, label='Kinetic Energy')
+    plt.plot(sol.t, total_energy, label='Total Energy')
+    plt.xlabel('Time (s')
+    plt.ylabel('Energies (J)')
+    plt.title(f'Change in energies | vy0 = {start_vel}')
+    plt.savefig(f'energy_{start_vel}.png')
